1a449992187c0e8d050b2bcc4eee7b88c5d5fa64
Jonas Behmer <jonas.behmer@callistaenterprise.se>
2014-01-07 09:18:32 +0100

    Cleaning up before adding proper example

---
+++ grails-app/controllers/grails/cadec/TweetController.groovy
@@ -0,0 +1,11 @@
+package grails.cadec
+
+import grails.transaction.*
+import static org.springframework.http.HttpStatus.*
+import static org.springframework.http.HttpMethod.*
+import grails.converters.*
+
+
+class TweetController {
+
+}
--- src/groovy/grails.cadec/TweetCommand.groovy
+++ src/groovy/grails.cadec/TweetCommand.groovy
@@ -4,9 +4,6 @@ import org.grails.databinding.BindUsing
 class TweetCommand {
     String status
 
-    //When we are using the BindUsing we can do stuff like pre-fetching other domains
-    //Of course we don't really need a Command object for this trivial case...but
-    //it is nice to show it anyway
     @BindUsing({ obj, src ->
         Account.get(src['userId']) })
     Account userId
